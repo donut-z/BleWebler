96eec8c57733360376efe0969ff517b1490c4edb
Daan Zillen <70492753+donut-z@users.noreply.github.com>
2026-03-15 18:47:48 +0100

    Update grayscale calculation to use luminosity

--- js/dithering.js
+++ js/dithering.js
@@ -8,7 +8,7 @@
 function toGrayscale(imageData) {
     const data = imageData.data;
     for (let i = 0; i < data.length; i += 4) {
-        const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        const avg = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
         data[i] = avg;     // red
         data[i + 1] = avg; // green
         data[i + 2] = avg; // blue
